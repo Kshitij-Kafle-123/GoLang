4c9e495134af84afdb2057d31a0f2a3994e363e2
kshitij kafle123 <kshitij.kafle123@gmail.com>
2022-01-06 21:04:56 +0545

    changes

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello GoLang")
-	fmt.Print("hello")
+	//fmt.Print("hello")
+
+	var anyThing string = "Hello"
+	var student = "student"
+	abc := 235
+	fmt.Println(abc)
+	fmt.Println(anyThing)
+	fmt.Println(student)
+
+	var a string
+	var b int
+	var c bool
+	var d float32
+	var e float64
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
 }
