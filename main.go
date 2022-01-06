a448dff673ec5c3736036d826474b37219aa710e
kshitij kafle123 <kshitij.kafle123@gmail.com>
2022-01-06 23:01:36 +0545

    Go Lang Basics Updated

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
+var abcd string
+
 func main() {
 	fmt.Println("Hello GoLang")
 	//fmt.Print("hello")
@@ -14,7 +16,8 @@ func main() {
 	fmt.Println(abc)
 	fmt.Println(anyThing)
 	fmt.Println(student)
-
+	abcd = "abc"
+	fmt.Println(abcd)
 	var a string
 	var b int
 	var c bool
@@ -26,4 +29,172 @@ func main() {
 	fmt.Println(c)
 	fmt.Println(d)
 	fmt.Println(e)
+
+	var x, y, z int = 14, 15, 16
+	var w, m, n = 500, "hello", 3.14
+	fmt.Println(w, m, n)
+	fmt.Println(x, y, z)
+	println(x)
+	const PI float32 = 3.14
+	fmt.Println(PI)
+
+	const (
+		Q   = 4.5
+		AQ  = 8.9
+		WWW = "World Wide Web"
+	)
+
+	fmt.Println(WWW)
+
+	var name string = "Kshitij Kafle."
+	var age int = 23
+
+	fmt.Printf("My Name is: %v and My Age is: %v", name, age)
+
+	var test = 100.25
+	var test1 = "anything testing"
+
+	fmt.Printf("Value: %v \n", test)
+	fmt.Printf("Go %#v \n", test1)
+	fmt.Printf("Type: %T\n", test1)
+	fmt.Printf("Percent Sign: %v%%\n", test)
+
+	var testArray = [3]int{1, 2, 3}
+	fmt.Println(testArray)
+	var inferedArray = [...]int{4, 5, 6, 7, 8}
+	fmt.Println(inferedArray)
+	arrayIsArray := [...]int{5, 7, 9}
+	fmt.Println(arrayIsArray)
+	var arrayString = [...]string{"abc", "def", "ghi"}
+	fmt.Println(arrayString)
+
+	arr1 := [...]int{1, 2}
+	arr2 := [3]int{}
+	arr3 := [3]int{2, 3}
+
+	fmt.Println(arr1, arr2, arr3)
+	if arr2[2] == 0 {
+		fmt.Println("In IF CONDITION")
+	}
+	//This initialize position 2nd and 5th (1 and 4)
+	amazingArray := [5]int{1: 15, 4: 58}
+	fmt.Println(amazingArray)
+	fmt.Println("Length of the array: ", len(amazingArray))
+
+	newSlice := []int{1, 2, 3}
+	fmt.Println("Slice in GOLANG: ", newSlice)
+
+	//Make a slice from an Array
+
+	var arrBeforeSlice = [7]int{4, 2, 6, 5, 5, 6, 8}
+	sliceFromArr := arrBeforeSlice[2:5]
+	fmt.Println("Slice created from Array: ", sliceFromArr, "Length: ", len(sliceFromArr), "Capacity: ", cap(sliceFromArr))
+	anotherSlice := arrBeforeSlice[:5]
+	fmt.Println("New Slice: ", anotherSlice)
+
+	//Creating a Slice from a make()  function
+
+	newSliceforMake := make([]int, 6, 10)
+	fmt.Println("Slice from make function ", newSliceforMake)
+
+	//appending element in a slice
+
+	appendSlice := []int{7, 5, 5, 8}
+	fmt.Println("Append Slice before appending: ", appendSlice)
+	appendSlice = append(appendSlice, 89, 56)
+	fmt.Println("Append Slice after appending: ", appendSlice)
+	sliceAnother := append(appendSlice, appendSlice...)
+	fmt.Println("Two slices: ", sliceAnother)
+
+	//Memory efficiency
+	heavySlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 5, 62, 216, 35, 8, 35, 225}
+	fmt.Println("Memory Consuming Slice: ", heavySlice)
+	neededElemOfHeavySlice := heavySlice[1 : len(heavySlice)-5]
+	copyOfSlice := make([]int, len(neededElemOfHeavySlice))
+	copy(copyOfSlice, neededElemOfHeavySlice)
+	fmt.Println("Copy Of Heavy Slice: ", copyOfSlice)
+
+	var operatorr = 10
+	operatorr += 520
+	fmt.Println("After Addition of 520: ", operatorr)
+
+	var less = 5
+	var more = 10
+	fmt.Println(less < more)
+
+	//LOGIC AND OR
+	var binX = 9
+	var binY = 10
+	fmt.Printf("x in binary or base2 : %b\n", binX)
+	fmt.Printf("y in binary or base2 : %b\n", binY)
+
+	//Use AND-Gate (Returns 1 if both bits are 1 otherwise returns 0)
+	fmt.Printf("binX AND binY (binX & binY): %b\n", binX&binY)
+	//Returns 1 if one of the bit is 1
+	fmt.Printf("binX OR binY (binX | binY): %b\n", binX|binY)
+
+	fmt.Printf("XOR: (binX ^ binY): %b\n", binX^binY)
+
+	//shift
+	fmt.Printf("Shift by 2: binX<<2: %b\n", binX<<2)
+	fmt.Printf("Shift by 2: binX>>2: %b\n", binX>>2)
+
+	//switch
+	dayOftheWeek := 3
+	switch dayOftheWeek {
+	case 1:
+		fmt.Println("Sunday")
+	case 2:
+		fmt.Println("Monday")
+	default:
+		fmt.Println("Not a day")
+	}
+	//Switch MultiCase
+	validNums := 9
+	switch validNums {
+	case 1, 2, 3, 4, 5:
+		fmt.Println("Valid Number")
+	case 8, 9:
+		fmt.Println("Invalid Number")
+	}
+
+	//for loops
+	for i := 0; i < 5; i++ {
+		fmt.Printf("For loop index in Binary: %b\n", i)
+	}
+	//range keyWord in looping
+	data := []string{"hello", "learn", "golang", "better", "way"}
+	for index, value := range data {
+		fmt.Printf("index: %v\t Value: %v\n", index, value)
+	}
+	for _, val := range data {
+		fmt.Printf("index: is N/A \t value is: %v\n", val)
+	}
+	newFunction(256)
+	errOrFunction(fmt.Errorf("error: 404"))
+	var result = sumOfTwoInt(10, 14)
+	fmt.Println("output from sum  function: ", result)
+	fmt.Println("Calling achhamma ko function: ", achhamma(45, 4.5))
+
+	fmt.Println(iLoveThisFunction(10, "Hello"))
+	nums, txt := iLoveThisFunction(5, "hello")
+	fmt.Println("values: ", nums, txt)
+}
+func newFunction(abc int) {
+	fmt.Println("i'm in new Function: Thank You for Calling Me !! the value you passed to me as a gift is : ", abc)
+}
+func errOrFunction(err error) {
+	fmt.Println("What the Error: ", err)
+}
+func sumOfTwoInt(x int, y int) int {
+	return x + y
+}
+func achhamma(x int, y float32) (kHoYesto float32) {
+	kHoYesto = float32(x) + y
+	return
+}
+func iLoveThisFunction(x int, y string) (num int, txt string) {
+	num = x + x
+	txt = y + " World!!"
+	return
 }
